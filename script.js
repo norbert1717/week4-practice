895dbecfc669ca8d072192f43040603f4bd56072
norbert1717 <miklosnorbert17@gmail.com>
2024-05-31 14:57:50 +0200

    added callback and arrow function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+/*console.log("hello world");
 
 // függvények
 
@@ -23,3 +23,34 @@ const coolerHelloWorld = function() { // ha változóba mentem a függvényem, a
 }
 
 coolerHelloWorld();
+
+const coolestHelloWorld = () => {
+    console.log("hello world from the coolest function");
+}
+
+coolestHelloWorld(); */
+
+function sumOfTwoNumbers(number1, number2) {
+    return number1 + number2;
+    // number1 + number2;  az érték undefined lesz
+}
+
+/*const result = sumOfTwoNumbers(1, 2);
+console.log("result: " + result);*/
+
+function multiplyTwoNumbers(number1, number2) {
+    return number1 * number2;
+}
+
+console.log(sumOfTwoNumbers(20, 2));
+
+const arrowSumOfTwoNumbers = (number1, number2) => number1 + number2; // a nyíl függvénynél nem szükséges a return szó, nem szükséges a kapcsos zárójel se. több műveletnél kell a kapcsos zárójel
+
+console.log(arrowSumOfTwoNumbers(1, 2));
+
+function doSomeMagic (number1, number2, callback) { // a callback elkér egy függvényt és majd valamikor lefuttatja
+    return callback(number1, number2);
+} 
+
+console.log(doSomeMagic(1, 2, sumOfTwoNumbers));
+console.log(doSomeMagic(1, 2, multiplyTwoNumbers));
