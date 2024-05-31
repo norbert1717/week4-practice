9d38d70433da2e50dc53a6d83a83f48f03aa4397
norbert1717 <miklosnorbert17@gmail.com>
2024-05-31 14:30:20 +0200

    added hello world functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello")
+console.log("hello world");
+
+// függvények
+
+helloWorld(); // már azelőtt meghívhatom a függvényt, mielőtt megírtam vollna, ha nem változóként hozom létre, felkúszik a 0. sorba a függvény.
+
+function helloWorld () {
+    console.log("hello world from the function");
+    helperFunction();
+    
+    function helperFunction() {
+        console.log("i am helping");
+    }
+    
+    const functionVariable = "text";
+    console.log(functionVariable);
+}
+    
+    console.log("hello world2");
+    
+const coolerHelloWorld = function() { // ha változóba mentem a függvényem, akkor később kapja meg a feladatát
+    console.log("hello world from the cooler function");
+}
+
+coolerHelloWorld();
